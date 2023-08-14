9b9787cdaed44b5075d12467a5f4500af7a79b5a
Vladimir Rodchenko <vladimir@rodchenko.by>
2023-08-14 18:24:09 +0200

    #0 improve pre-push dotnet stryker

--- dotnet/Stryker/validation.sh
+++ dotnet/Stryker/validation.sh
@@ -9,7 +9,7 @@ if [[ -z $VAR_DOTNET_STRYKER_PROJECT_PATHS ]]; then
 fi
 
 for projectPath in ${VAR_DOTNET_STRYKER_PROJECT_PATHS[@]}; do
-    ( cd $projectPath && dotnet stryker --mutation-level $VAR_DOTNET_STRYKER_MUTATION_LEVEL --break-at $VAR_DOTNET_STRYKER_SCORE_MIN )
+    ( cd $projectPath && dotnet stryker --mutation-level $VAR_DOTNET_STRYKER_MUTATION_LEVEL --break-at $VAR_DOTNET_STRYKER_SCORE_MIN --since:$VAR_DOTNET_STRYKER_SINCE_BRANCH )
 
     if [[ $? -ne 0 ]]; then
         echo -e "${COLOR_RED}Stryker score '$projectPath' is lower than $VAR_DOTNET_STRYKER_SCORE_MIN.${COLOR_NO}"
