4de64bc3006d8e288099f9049dedf91f2b946806
Vladimir Rodchenko <vladimir@rodchenko.by>
2024-07-08 17:38:20 +0200

    #0 dotnet stryker if not project then solution

--- dotnet/Stryker/validation.sh
+++ dotnet/Stryker/validation.sh
@@ -4,15 +4,19 @@
 dotnet tool install --global dotnet-stryker
 
 if [[ -z $VAR_DOTNET_STRYKER_PROJECT_PATHS ]]; then
-    echo -e "${COLOR_RED}Stryker project list is empty!${COLOR_NO}"
-    exit 1
-fi
-
-for projectPath in ${VAR_DOTNET_STRYKER_PROJECT_PATHS[@]}; do
-    ( cd $projectPath && dotnet stryker --mutation-level $VAR_DOTNET_STRYKER_MUTATION_LEVEL --break-at $VAR_DOTNET_STRYKER_SCORE_MIN --since:$VAR_DOTNET_STRYKER_SINCE_BRANCH )
+    ( dotnet stryker --mutation-level $VAR_DOTNET_STRYKER_MUTATION_LEVEL --break-at $VAR_DOTNET_STRYKER_SCORE_MIN --since:$VAR_DOTNET_STRYKER_SINCE_BRANCH )
 
     if [[ $? -ne 0 ]]; then
         echo -e "${COLOR_RED}Stryker score '$projectPath' is lower than $VAR_DOTNET_STRYKER_SCORE_MIN.${COLOR_NO}"
         exit 1
     fi
-done
+else
+    for projectPath in ${VAR_DOTNET_STRYKER_PROJECT_PATHS[@]}; do
+        ( cd $projectPath && dotnet stryker --mutation-level $VAR_DOTNET_STRYKER_MUTATION_LEVEL --break-at $VAR_DOTNET_STRYKER_SCORE_MIN --since:$VAR_DOTNET_STRYKER_SINCE_BRANCH )
+
+        if [[ $? -ne 0 ]]; then
+            echo -e "${COLOR_RED}Stryker score '$projectPath' is lower than $VAR_DOTNET_STRYKER_SCORE_MIN.${COLOR_NO}"
+            exit 1
+        fi
+    done
+fi
